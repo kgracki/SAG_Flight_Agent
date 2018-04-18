fbfac41f99ad0d0e4dd8c557c9c84c9aba52b3fc
Kacper <kacpergracki@gmail.com>
2018-04-18 14:31:16 +0200

    Small changes

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from consumer import *
 from flight_webscrap import check_flights
 from email_notification import send_email
 
-        
+
 empty_model = MoneyModel(10)
 for i in range(2):
     empty_model.step()
+
 check_flights(3, 6)
 send_email("284")
 
